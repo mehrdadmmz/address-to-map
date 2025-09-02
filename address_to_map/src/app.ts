0e645c55696425d2e4146c188ea89ba5c16cea70
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2025-09-01 20:53:58 -0700

    finished code for the address to mpa mini project in TS

---
+++ address_to_map/src/app.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+const GOOGLE_API_KEY = "AIzaSyAlMANpfMeXFWXKUQyVdh_E36ibU58e-KY";
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  // convert to URL compatible string
+  const urlCompatibleEnteredAddress = encodeURI(enteredAddress);
+
+  // send this to Google's API!
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${urlCompatibleEnteredAddress}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+  const map = new google.maps.Map(document.getElementById("map")!, {
+        center: coordinates,
+        zoom: 16,
+      });
+
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form.addEventListener("submit", searchAddressHandler);
---
+++ quickstart/src/main/java/com/devtiro/quickstart/HelloWorldController.java
@@ -0,0 +1,4 @@
+package com.devtiro.quickstart;
+
+public class HelloWorldController {
+}
